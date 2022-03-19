11dd84c923c2c18d960f6fbf22cfe6a7b8c003ce
MiharuNamiki <miharu.namiki.5x@gmail.com>
2022-03-19 23:45:01 +0900

    それぞれ別々のファイルを生成

---
+++ takumi.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+import tkinter.ttk as ttk
+
+
+# takumi
+def tab2_main(tab2):
+    #文字を表示する。
+    param_name = tk.Label(tab2, text="タブ2の内容")
+    param_name.place(x=10, y=20)
+    return 0
