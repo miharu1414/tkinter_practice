706406644524f53bf53a6d97d129ad72e81ef0ee
takumi <88089834+takumi-kawauchi@users.noreply.github.com>
2022-03-21 23:10:28 +0900

    Update takumi.py

--- takumi.py
+++ takumi.py
@@ -2,8 +2,8 @@ import tkinter as tk
 import tkinter.ttk as ttk
 
 
-# takumi
 def tab2_main(tab2):
+    
     from tkinter import messagebox as mb
     #入力内容をcsvに保存
     def shows():
@@ -22,9 +22,10 @@ def tab2_main(tab2):
             fixed_l = l.rstrip()
             rows.append(fixed_l.split(','))
             l = f_r.readline()
-        for i in rows :
+        for i in range(1,len(rows)) :
                 user_write = tk.Label(tab2, 
-                                      text='ユーザー名：' + i[0] +' /内容：' + i[1], 
+                                      text='ユーザー名：' + rows[i][0] +' \n内容：' + rows[i][1], 
+                                      relief = 'ridge',
                                       fg = 'orange'
                                       )
                 sep = ttk.Separator(tab2, 
@@ -32,44 +33,48 @@ def tab2_main(tab2):
                                     style="blue.TSeparator",
                                     )
                 sep.pack(fill="both")
-                user_write.pack()    
+                user_write.pack()
+                
         f_r.close()
     
     #伝言入力フォームの挿入。
+   
     user_name = tk.Label(tab2, 
-                         text="ユーザー名を入力", 
-                         foreground = '#ff0000'
-                         )
+                          text="ユーザー名を入力", 
+                          foreground = '#ff0000'
+                          )
     user_name.pack()
-  
+    
     user = tk.Entry(tab2, 
-                    justify= 'center', 
-                    width=50,
-                    )
+                     #justify= 'center', 
+                     width=50,
+                     )
+    
     user.pack()
     
     mess_name = tk.Label(tab2, 
-                         text="伝言を入力", 
-                         foreground = '#ff0000'
-                         )
+                          text="伝言を入力", 
+                          foreground = '#ff0000'
+                          )
     mess_name.pack()
     
     mess = tk.Entry(tab2, 
-                    width=50,
-                    )
+                     width=50,
+                     )
+    
     mess.pack()
     
     #ボタン
     mess_button = tk.Button(tab2, 
-                            text=u'投稿',
-                            command = shows
-                            )
+                       text=u'投稿',
+                       command = shows
+                       )
     mess_button.pack()
 
     write_button = tk.Button(tab2, 
-                             text=u'投稿内容を見る',
-                             command = write
-                             )
+                       text=u'投稿内容を見る',
+                       command = write
+                       )
     write_button.pack()
     
     tab2.mainloop()
